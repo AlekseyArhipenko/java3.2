7bba04da8353f784d57e4b92a54b742570ee0190
Aleksey Arkhipenko <arhipenko1990@inbox.ru>
2021-12-05 12:21:33 +0300

    Code

--- crc/src/BmiService.java
+++ crc/src/BmiService.java
@@ -1,4 +1,4 @@
-public class BodyMassIndex {
+public class BmiService {
     public float calculate(float height, int weight) {
         float index = weight / (height * height);
         return (float) index;
--- crc/src/Main.java
+++ crc/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        BodyMassIndex service = new BodyMassIndex();
-        float height = (float) 1.58;
-        int weight = 69;
+        BmiService service = new BmiService();
+        float height = (float) 1.85;
+        int weight = 90;
         float index = service.calculate(height, weight);
         System.out.println(index);
 
